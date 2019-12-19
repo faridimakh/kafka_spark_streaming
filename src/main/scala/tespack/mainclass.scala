bedf06bd14b03e3e69469531f560804b6cbd25be
faridimakh <secintfar1A>
2019-12-19 01:53:56 +0100

    "seconde commit elastic"

--- src/main/scala/tespack/mainclass.scala
+++ src/main/scala/tespack/mainclass.scala
@@ -3,7 +3,7 @@ package tespack
 object mainclass {
   def main(args: Array[String]): Unit = {
 
-    pull_data_from_vilib_Api_to_ELK(3000,1500)
+    pull_data_from_vilib_Api_to_ELK(4000,20000)
 
   }
 
